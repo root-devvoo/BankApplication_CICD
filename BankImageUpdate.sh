322f01589ab9e8b4b11f450b86352fad9e7c3436
Jenkins <jenkins@ip-172-31-8-239.ap-northeast-2.compute.internal>
2023-05-11 07:46:01 +0000

    /var/lib/jenkins/workspace/cicd

--- BankImageUpdate.sh
+++ BankImageUpdate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 URI="015501295117.dkr.ecr.ap-northeast-2.amazonaws.com/bankapp:Backend"
-FILE="/home/ubuntu/cicd/bankapp-api-deployment-service.yaml"
+FILE="bankapp-api-deployment-service.yaml"
 
 sed -i "s#$URI*#$URI$1#g" $FILE
+git add $FILE
+git commit -m '[UPDATE] bankapp:Backend${BUILD_NUMBER} image versioning'
+git push origin main
