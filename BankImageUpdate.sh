eaafb64b7371fb869566856de48227448a7b21ea
Woocheol Jung <jwcnet93@gmail.com>
2023-05-11 09:38:07 +0000

    update (2)

--- BankImageUpdate.sh
+++ BankImageUpdate.sh
@@ -2,7 +2,7 @@
 URI="015501295117.dkr.ecr.ap-northeast-2.amazonaws.com/bankapp:Backend"
 FILE="bankapp-api-deployment-service.yaml"
 
+cd /var/lib/jenkins/workspace/cicd
+echo "$URI$1"
+echo "$1"
 sed -i "s#$URI*#$URI$1#g" $FILE
-git add $FILE
-git commit -m '[UPDATE] bankapp:Backend${BUILD_NUMBER} image versioning'
-git push origin main
